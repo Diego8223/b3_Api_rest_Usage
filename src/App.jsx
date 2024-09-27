0155ff37f1e4a5c6629560bfaf7092a46cbf9b34
Diego.ruiz <dianruro8223@otmail.com>
2024-09-26 20:26:14 -0500

    commit5

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { ApiCoffeeCarruselBasic } from './assets/components/AppicoffeecarruselBasic'
+
+function App() {
+
+
+  return (
+       <div>
+       <ApiCoffeeCarruselBasic/>
+    </div>
+  );
+};
+
+export default App
